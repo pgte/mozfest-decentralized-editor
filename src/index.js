0bacaba1b6a06826652b1e93f142decdb3ca810f
Pedro Teixeira <i@pgte.me>
2017-10-24 09:37:11 +0100

    connected example improved

--- src/index.js
+++ src/index.js
@@ -1,107 +1,137 @@
 'use strict';
 
-// const Y = require('yjs')
-// require('y-array')(Y)
-// require('y-memory')(Y)
-// require('y-indexeddb')(Y)
-// require('y-ipfs-connector')(Y)
-
-// const IPFS = require('ipfs')
-
-// const ipfs = new IPFS({
-//   EXPERIMENTAL: {
-//     pubsub: true
-//   }
-// })
+// ------ Y.js: import and wire dependencies --------
+const Y = require('yjs')
+require('y-array')(Y)
+require('y-memory')(Y)
+require('y-indexeddb')(Y)
+require('y-ipfs-connector')(Y)
 
 const d3 = require('d3')
 
-// ipfs.once('start', async () => {
-  // console.log('IPFS started')
+// ------ IPFS node creation ------
+const IPFS = require('ipfs')
+const ipfs = new IPFS({
+  EXPERIMENTAL: {
+    pubsub: true
+  }
+})
 
-  // const y = await Y({
-  //   db: {
-  //     name: 'indexeddb'
-  //   },
-  //   connector: {
-  //     name: 'ipfs',
-  //     room: 'mozfest-flipchart',
-  //     ipfs: ipfs
-  //   },
-  //   share: {
-  //     flipchart: 'Array'
-  //   }
-  // })
-  // console.log('Y started')
-  // var drawing = y.share.flipchart
+// ------ Wait for IPFS to start ------
+ipfs.once('start', ipfsStarted)
 
+async function ipfsStarted () {
+  console.log('IPFS started')
+
+
+  // ------ Y.js: Initialize CRDT ------
+  const y = await Y({
+    db: {
+      name: 'indexeddb'
+    },
+    connector: {
+      name: 'ipfs',
+      room: 'mozfest-flipchart',
+      ipfs: ipfs
+    },
+    share: {
+      flipchart: 'Array'
+    }
+  })
+
+  // ------ IPFS: print Peer Id ------
+  ipfs.id(haveIPFSId)
+
+  function haveIPFSId (err, peerId) {
+    if (err) { throw err }
+    document.getElementById('status').innerHTML = 'Started. Peer Id is ' +  peerId.id
+  }
+
+  var drawing = y.share.flipchart
+
+  // ------ D3: translate line point into D3 render path
   var renderPath = d3.line()
     .x(function (d) { return d[0] })
     .y(function (d) { return d[1] })
     .curve(d3.curveMonotoneX)
 
+  // ------ D3 Flipchart drawing initialization ------
   var svg = d3.select('#flipchart')
-    .call(d3.drag()
-      .on('start', dragstart)
-      .on('drag', drag)
-      .on('end', dragend))
 
-  // create line from a shared array object and update the line when the array changes
-  // function drawLine (yarray) {
-  //   var line = svg.append('path')
-  //     .datum(yarray.toArray())
-  //     .attr('class', 'line')
+  // ------ CRDT and D3: Draw a new line ------
+  function drawLine (yarray) {
+    var line = svg.append('path')
+      .datum(yarray.toArray())
+      .attr('class', 'line')
 
-  //   line.attr('d', renderPath)
-  //   yarray.observe(function (event) {
-  //     // we only implement insert events that are appended to the end of the array
-  //     event.values.forEach(function (value) {
-  //       line.datum().push(value)
-  //     })
-  //     line.attr('d', renderPath)
-  //   })
-  // }
+    line.attr('d', renderPath)
 
-  // call drawLine every time an array is appended
-  // y.share.flipchart.observe(function (event) {
-  //   if (event.type === 'insert') {
-  //     event.values.forEach(drawLine)
-  //   } else {
-  //     // just remove all elements (thats what we do anyway)
-  //     svg.selectAll('path').remove()
-  //   }
-  // })
+    // Observe changes that happen on this line
+    yarray.observe(lineChanged)
 
-  // draw all existing content
-  // for (var i = 0; i < drawing.length; i++) {
-  //   drawLine(drawing.get(i))
-  // }
+    function lineChanged(event) {
+      // we only implement insert events that are appended to the end of the array
+      event.values.forEach(function (value) {
+        line.datum().push(value)
+      })
+      line.attr('d', renderPath)
+    }
+  }
+
+  // ------ CRDT: listen for new and removed lines ------
+  drawing.observe(drawingChanged)
+
+  function drawingChanged (event) {
+    if (event.type === 'insert') {
+      event.values.forEach(drawLine)
+    } else {
+      // just remove all elements (thats what we do anyway)
+      svg.selectAll('path').remove()
+    }
+  }
+
+  // ------ CRDT: draw all existing content ------
+  for (var i = 0; i < drawing.length; i++) {
+    drawLine(drawing.get(i))
+  }
+
+  // ------ User interaction: handle drag events ------
+
+  svg.call(d3.drag()
+    .on('start', dragStarted)
+    .on('drag', dragged)
+    .on('end', dragEnded))
 
   var sharedLine = null
-  function dragstart () {
-    // drawing.insert(drawing.length, [Y.Array])
-    // sharedLine = drawing.get(drawing.length - 1)
 
-    sharedLine = svg.append('path')
-      .datum([])
-      .attr('class', 'line')
+  function dragStarted () {
+    drawing.insert(drawing.length, [Y.Array])
+    sharedLine = drawing.get(drawing.length - 1)
   }
 
   // After one dragged event is recognized, we ignore them for 33ms.
   var ignoreDrag = null
-  function drag () {
+  function dragged () {
     if (sharedLine && !ignoreDrag) {
       ignoreDrag = window.setTimeout(function () {
         ignoreDrag = null
       }, 33)
-      sharedLine.datum().push(d3.mouse(this))
-      sharedLine.attr('d', renderPath)
+      const mouse = d3.mouse(this)
+      sharedLine.push([mouse])
     }
   }
 
-  function dragend () {
+  function dragEnded () {
     sharedLine = null
     window.clearTimeout(ignoreDrag)
     ignoreDrag = null
   }
-// })
+
+  // ------ User interaction: Clear all ------
+
+  document.getElementById('clear').onclick = clickedClear
+
+  function clickedClear() {
+    drawing.delete(0, drawing.length)
+  }
+}
